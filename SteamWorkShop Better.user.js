d26495d4e15b7fe3d50c8b5d3270993b70d1b1b7
SmileyZomb <32229508+SmileyZomb@users.noreply.github.com>
2022-01-20 18:31:38 +0100

    Update SteamWorkShop Better.user.js

--- SteamWorkShop Better.user.js
+++ SteamWorkShop Better.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SteamWorkShop Better
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       SmileyZomb
 // @match        https://steamcommunity.com/sharedfiles/managecollection/?id=*
